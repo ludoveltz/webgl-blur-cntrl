03b4ce4488ac3813f2f991d4720cf0b9f607c39e
Ludovic <ludoveltz@gmail.com>
2025-07-18 22:48:54 +0200

    Ultra-simplified version - removed all problematic dependencies and configs

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true
-  },
-  webpack: (config) => {
-    // Configuration pour les shaders GLSL
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      type: 'asset/source',
-    });
-    return config;
-  },
+  }
 };
 
 export default nextConfig; 
